ed3b1fb15f9dd958223260ca3da962f3aaeb6948
Zuber Konnur <zuberbkonnur786@gmail.com>
2025-09-10 11:39:19 +0530

    solved the error for build error

--- frontend/src/entry.vercel-edge.tsx
+++ frontend/src/entry.vercel-edge.tsx
@@ -7,15 +7,19 @@
  * - https://qwik.dev/docs/deployments/vercel-edge/
  *
  */
-import {
-  createQwikCity,
-  type PlatformVercel,
-} from "@builder.io/qwik-city/middleware/vercel-edge";
-import qwikCityPlan from "@qwik-city-plan";
-import render from "./entry.ssr";
 
-declare global {
-  interface QwikCityPlatform extends PlatformVercel {}
-}
+//commented this for now to fix build error
+// import {
+//   createQwikCity,
+//   type PlatformVercel,
+// } from "@builder.io/qwik-city/middleware/vercel-edge";
+// import qwikCityPlan from "@qwik-city-plan";
+// import render from "./entry.ssr";
+
+// declare global {
+//   interface QwikCityPlatform extends PlatformVercel {}
+// }
+
+
 
 export default createQwikCity({ render, qwikCityPlan });
