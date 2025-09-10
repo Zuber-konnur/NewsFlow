2b3df0a3211664a4a8c0e76095f3c8d1f3c77c1d
Zuber Konnur <zuberbkonnur786@gmail.com>
2025-09-10 22:53:33 +0530

    add entry point for running the FastAPI application with dynamic port configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,3 +8,9 @@ setup_cors(app)
 app.include_router(auth.router, prefix="/api/auth", tags=["Auth"])
 app.include_router(news.router, prefix="/api/news", tags=["News"])
 app.include_router(user.router, prefix="/api/user", tags=["User"])
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get('PORT', 8000))
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=port)
